c464dd9045e1341a724b31b0bd112b03c8f8207c
Shashank Gonchigar <43459668+gonchigars@users.noreply.github.com>
2024-01-04 16:12:29 +0530

    Update GreetTest.java

--- src/test/GreetTest.java
+++ src/test/GreetTest.java
@@ -1,20 +1,20 @@
 package src.test;
 
-import org.junit.*;
-import static org.junit.Assert.*;
-import src.main.Greet; // Importing the main code
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import src.main.Greet; // Importing the main class
 
 public class GreetTest {
-    
+
     @Test 
     public void testSaysMethod() {
         Greet gre = new Greet();
-        assertEquals("Helloo", gre.says());
+        assertEquals("Helloo", gre.says(), "The says method should return 'Helloo'");
     }
 
     @Test
     public void twoPlusTwoShouldEqualFour() {
         Greet gre1 = new Greet();
-        assertEquals(4, gre1.add(2, 2));
+        assertEquals(4, gre1.add(2, 2), "Adding 2 and 2 should equal 4");
     }
 }
